94e8ed880abd0805898b380320e3807cd5c7d912
CientistaVuador <52110280+CientistaVuador@users.noreply.github.com>
2022-12-14 23:54:44 -0300

    Added Window, Game Loop, OpenSimplex Noise, Raw Mouse Motion

--- src/main/java/cientistavuador/ciencraftreal/Main.java
+++ src/main/java/cientistavuador/ciencraftreal/Main.java
@@ -27,6 +27,9 @@
 package cientistavuador.ciencraftreal;
 
 import static org.lwjgl.glfw.GLFW.*;
+import org.lwjgl.opengl.GL;
+import static org.lwjgl.opengl.GL33C.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 /**
  * Main class
@@ -38,11 +41,113 @@ public class Main {
         org.lwjgl.system.Configuration.LIBRARY_PATH.set("natives");
     }
     
+    public static class OpenGLErrorException extends RuntimeException {
+        
+        private static final long serialVersionUID = 1L;
+        private final int error;
+        
+        public OpenGLErrorException(int error) {
+            super("OpenGL Error "+error);
+            this.error = error;
+        }
+
+        public int getError() {
+            return error;
+        }
+        
+    }
+    
+    public static boolean THROW_GL_ERRORS = true;
+    public static void checkGLError() {
+        int error = glGetError();
+        if (error != 0) {
+            OpenGLErrorException err = new OpenGLErrorException(error);
+            if (THROW_GL_ERRORS) {
+                throw err;
+            } else {
+                err.printStackTrace(System.err);
+            }
+        }
+    }
+    
+    public static double TPF = 1/60d;
+    public static int FPS = 60;
+    public static long WINDOW_POINTER = NULL;
+    public static final Game GAME = new Game();
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        glfwInit();
+        if (!glfwInit()) {
+            throw new IllegalStateException("Could not initialize GLFW!");
+        }
+        
+        //glfwWindowHint(GLFW_SAMPLES, 4); //MSAA 4x
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        
+        WINDOW_POINTER = glfwCreateWindow(800, 600, "CienCraft - FPS: 60", NULL, NULL);
+        if (WINDOW_POINTER == NULL) {
+            throw new IllegalStateException("Could not create a OpenGL 3.3 Context Window! Update your drivers or buy a new GPU.");
+        }
+        glfwMakeContextCurrent(WINDOW_POINTER);
+        
+        glfwSwapInterval(1);
+        
+        if (glfwRawMouseMotionSupported()) {
+            glfwSetInputMode(WINDOW_POINTER, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+        }
+        
+        GL.createCapabilities();
+        
+        glfwSetFramebufferSizeCallback(WINDOW_POINTER, (window, width, height) -> {
+            glViewport(0, 0, width, height);
+            GAME.windowSizeChanged(width, height);
+            Main.checkGLError();
+        });
+        
+        //glEnable(GL_MULTISAMPLE);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glClearColor(0.2f, 0.4f, 0.6f, 1.0f);
+        glEnable(GL_DEPTH_TEST);
+        glClearDepth(1f);
+        glDepthFunc(GL_LEQUAL);
+        
+        Main.checkGLError();
+        
+        int frames = 0;
+        long nextFpsUpdate = System.currentTimeMillis() + 1000;
+        
+        GAME.start();
+        
+        Main.checkGLError();
+        
+        while (!glfwWindowShouldClose(WINDOW_POINTER)) {
+            long timeFrameBegin = System.nanoTime();
+            
+            glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+            glfwPollEvents();
+            
+            GAME.loop();
+            
+            glfwSwapBuffers(WINDOW_POINTER);
+            
+            Main.checkGLError();
+            
+            frames++;
+            if (System.currentTimeMillis() >= nextFpsUpdate) {
+                Main.FPS = frames;
+                frames = 0;
+                nextFpsUpdate = System.currentTimeMillis() + 1000;
+                
+                glfwSetWindowTitle(WINDOW_POINTER, "CienCraft - FPS: "+Main.FPS);
+            }
+            
+            Main.TPF = (System.nanoTime()-timeFrameBegin) / 1E6d;
+        }
         
         glfwTerminate();
     }
