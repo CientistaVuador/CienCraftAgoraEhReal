988d5542f73160f84342c4b80681d749c01d2e85
CientistaVuador <52110280+CientistaVuador@users.noreply.github.com>
2022-12-22 16:15:09 -0300

    Added debug calls

--- src/main/java/cientistavuador/ciencraftreal/Game.java
+++ src/main/java/cientistavuador/ciencraftreal/Game.java
@@ -26,11 +26,15 @@
  */
 package cientistavuador.ciencraftreal;
 
+import cientistavuador.ciencraftreal.block.Block;
 import cientistavuador.ciencraftreal.block.BlockRegister;
 import cientistavuador.ciencraftreal.block.Blocks;
 import cientistavuador.ciencraftreal.debug.Triangle;
 import cientistavuador.ciencraftreal.camera.FreeCamera;
+import cientistavuador.ciencraftreal.chunk.Chunk;
+import static cientistavuador.ciencraftreal.chunk.Chunk.CHUNK_SIZE;
 import cientistavuador.ciencraftreal.debug.DebugBlock;
+import cientistavuador.ciencraftreal.debug.DebugCounter;
 
 /**
  *
@@ -47,22 +51,66 @@ public class Game {
     private final Triangle triangle = new Triangle();
     private final FreeCamera camera = new FreeCamera();
     private final DebugBlock block = new DebugBlock();
+    private final Chunk chunk = new Chunk(65487321654L, 0, 0);
 
     private Game() {
 
     }
 
     public void start() {
+        chunk.generateBlocks();
+
+        DebugCounter counter = new DebugCounter("Chunk Generation Benchmark");
+
+        for (int i = 0; i < 1000; i++) {
+            counter.markStart("generation");
+            chunk.generateBlocks();
+            counter.markEnd("generation");
+        }
+
+        counter.print();
 
     }
 
+    int currentY = 255;
+    
     public void loop() {
         camera.updateMovement();
         triangle.render(camera.getProjection(), camera.getView());
+        block.copySideTextures(Blocks.STONE);
+
         for (int i = 1; i < BlockRegister.numberOfRegisteredBlocks(); i++) {
             block.getModel().identity().translate(1 + (i * 1), 0, 0);
             block.copySideTextures(BlockRegister.getBlock(i));
             block.render(camera.getProjection(), camera.getView());
+        }
+        
+        for (int z = 0; z >= -(CHUNK_SIZE - 1); z--) {
+            for (int x = 0; x < CHUNK_SIZE; x++) {
+                float worldX = (x + (chunk.getChunkX() * CHUNK_SIZE)) + 0.5f;
+                float worldY = currentY + 0.5f;
+                float worldZ = (-z + (chunk.getChunkZ() * CHUNK_SIZE)) - 0.5f;
+
+                Block blockAt = chunk.getBlockImpl(x, currentY, z);
+
+                if (blockAt == Blocks.AIR) {
+                    continue;
+                }
+
+                block.getModel()
+                        .identity()
+                        .translate(
+                                worldX,
+                                worldY,
+                                worldZ
+                        );
+                block.render(camera.getProjection(), camera.getView());
+            }
+        }
+        currentY--;
+        
+        if (currentY <= Chunk.GENERATOR_DESIRED_MIN_HEIGHT) {
+            currentY = Chunk.GENERATOR_DESIRED_MAX_HEIGHT;
         }
     }
 
